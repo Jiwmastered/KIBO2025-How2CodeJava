e59a5d9369cb1d5edeb9adda01c5552229ea0140
jiwmastered <nopparuj.rat@gmail.com>
2025-04-28 15:21:48 +0700

    Target area idenification
    add 3 variables
    1. target as String
    2. astroMarker as pair (2 target maker, assign as first and second value in the pair)
    3. patrolResult as  Map<String, List<Pair<String, String>>> (patroled marker as key, pair of treasure and area in the list as value)

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,5 +1,8 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
+import android.accessibilityservice.GestureDescription;
+import android.util.Pair;
+
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 import gov.nasa.arc.astrobee.types.Point;
@@ -7,13 +10,20 @@ import gov.nasa.arc.astrobee.types.Quaternion;
 
 import org.opencv.core.Mat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.Vector;
+
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
  */
 
 public class YourService extends KiboRpcService {
     @Override
-    protected void runPlan1(){
+    protected void runPlan1() {
         // The mission starts.
         api.startMission();
 
@@ -31,11 +41,45 @@ public class YourService extends KiboRpcService {
         /* ******************************************************************************** */
 
         // When you recognize landmark items, let’s set the type and number.
-        api.setAreaInfo(1, "item_name", 1);
 
         /* **************************************************** */
         /* Let's move to each area and recognize the items. */
-        /* **************************************************** */
+
+        // Use Map
+        Map<String, Pair<Point, Quaternion>> pointsMap = new HashMap<>();
+        String patrolPath[] = {"Area 1", "Area 2", "Area 3", "Area 4"};
+        List<String> sequencePath = new ArrayList<>(Arrays.asList(patrolPath));
+
+        Map<String, List<Pair<String, String>>> patrolResult = new HashMap<>();  // map[Markers] = list( pair<string, string> )
+
+        pointsMap.put("Area 1", new Pair<>(new Point(10.8d, -9.7d, 4.7d), new Quaternion(0.687483f, -0.164974f, 0.164974f, 0.687483f)));
+        if (pointsMap.isEmpty()) {
+            System.out.println("ERROR: 'pointsMap' is empty");
+            api.shutdownFactory();
+        }
+        pointsMap.put("Area 2", new Pair<>(new Point(10.9d, -8.8d, 4.6d), new Quaternion(0.5f, -0.5f, 0.5f, 0.5f)));
+        pointsMap.put("Area 3", new Pair<>(new Point(10.9d, -7.9d, 4.5d), new Quaternion(0.5f, -0.5f, 0.5f, 0.5f)));
+        pointsMap.put("Area 4", new Pair<>(new Point(10.6d, -6.7d, 5.0d), new Quaternion(0f, 0f, 0f, 1f)));
+        pointsMap.put("Area 5", new Pair<>(new Point(11.1d, -6.9d, 4.8d), new Quaternion(0f, 0f, -0.707f, 0.707f)));
+
+        /*
+        for (Map.Entry<String, Pair<Point, Quaternion>> entry : pointsMap.entrySet()) {*/
+        for (int i=0; i < sequencePath.size(); i++) {
+            String area = sequencePath.get(i);
+            Pair<Point, Quaternion> pair = pointsMap.get(area);
+
+            // Move through every area
+            point = pair.first;
+            quaternion = pair.second;
+            api.moveTo(point, quaternion, true);
+
+            // Capture markers and treasures
+            int itemQuant = 2; // Item Quantity
+            String itemName = "itemName"; // Item Name
+
+            api.setAreaInfo(i + 1, area, itemQuant);
+        }
+
 
         // When you move to the front of the astronaut, report the rounding completion.
         point = new Point(11.143d, -6.7607d, 4.9654d);
@@ -45,11 +89,41 @@ public class YourService extends KiboRpcService {
 
         /* ********************************************************** */
         /* Write your code to recognize which target item the astronaut has. */
+        String target = new String("crystal");
+        Pair<String, String> astroMarker = new Pair<>("coin", "compass"); // First and Second Target landmark
+        String targetArea = "Area 1";
+
+        if (patrolResult.containsKey(astroMarker.first)) {
+            List<Pair<String, String>> patrolList = patrolResult.get(astroMarker.first);
+            for (int i=0; i < patrolList.size(); i++) {
+                String treasure = patrolList.get(i).first;
+                if (target == treasure) {
+                    targetArea = patrolList.get(i).second;
+                }
+            }
+        } else if (patrolResult.containsKey(astroMarker.second)) {
+            List<Pair<String, String>> patrolList = patrolResult.get(astroMarker.second);
+            for (int i=0; i < patrolList.size(); i++) {
+                String treasure = patrolList.get(i).first;
+                if (target == treasure) {
+                    targetArea = patrolList.get(i).second;
+                }
+            }
+        } else {
+            System.out.print("No Marker Detected");
+        }
+        // If target is Area 2:
+
+
         /* ********************************************************** */
 
         // Let's notify the astronaut when you recognize it.
         api.notifyRecognitionItem();
 
+
+
+        Pair<Point, Quaternion> targetCoordinate = new Pair<>(pointsMap.get(targetArea).first, pointsMap.get(targetArea).second);
+        api.moveTo(targetCoordinate.first, targetCoordinate.second, false); // go to target area
         /* ******************************************************************************************************* */
         /* Write your code to move Astrobee to the location of the target item (what the astronaut is looking for) */
         /* ******************************************************************************************************* */
@@ -58,18 +132,30 @@ public class YourService extends KiboRpcService {
         api.takeTargetItemSnapshot();
     }
 
+    // No use
+    /*
     @Override
     protected void runPlan2(){
        // write your plan 2 here.
+        api.startMission();
+
+        Point point = new Point(10.9d, -9.92284d, 5.195d);
+        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+
+        point = new Point(11.143d, -6.7607d, 4.9654d);
+        quaternion = new Quaternion(0, 0, 0.5f, 0.5f);
+
     }
 
     @Override
     protected void runPlan3(){
         // write your plan 3 here.
     }
+    */
 
     // You can add your method.
-    private String yourMethod(){
+    private String yourMethod() {
         return "your method";
     }
 }
